ecc2353d44289d42dd0319dd5861b4a7f11a0fc5
Yakumo-zi <1477717283@qq.com>
2024-07-28 15:39:09 +0800

    feat:add grouped expression support

--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -61,6 +61,7 @@ func NewParser(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -226,6 +227,15 @@ func (p *Parser) peekPrecedence() int {
 		return p
 	}
 	return LOWEST
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+	exp := p.parseExpression(LOWEST)
+	if !p.expectToken(token.RPAREN) {
+		return nil
+	}
+	return exp
 }
 
 func (p *Parser) curPrecedence() int {
