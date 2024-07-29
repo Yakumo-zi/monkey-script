e835a8f4f35c6b2074b82207a94110699d981bf1
Yakumo-zi <1477717283@qq.com>
2024-07-29 13:26:27 +0800

    fix: env position error

--- interpreter/repl/repl.go
+++ interpreter/repl/repl.go
@@ -14,6 +14,7 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -28,7 +29,6 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		env := object.NewEnvironment()
 		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
